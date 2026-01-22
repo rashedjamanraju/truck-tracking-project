34e784c43cdc103c3706bec04259a0d443cbceb0
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2026-01-22 14:39:42 +0600

    Day-02: Setup React + Tailwind frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-slate-900 text-white text-2xl">
+      Tailwind Setup Successful 🚀
+    </div>
+  )
+}
